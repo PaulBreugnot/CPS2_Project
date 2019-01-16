a094ba46598a1670e3dc270a77a90065caeb1bff
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2019-01-16 23:49:40 +0100

    added authentification (Keycloak), need to be setup

--- territoire-core/app.js
+++ territoire-core/app.js
@@ -10,12 +10,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-var allowCrossDomain = function(req, res, next) {
-  console.log("Hello allow origin");
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+var allowCrossDomain = function (req, res, next) {
+    console.log("Hello allow origin");
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 }
 
 // view engine setup
@@ -25,7 +25,9 @@ app.set('view engine', 'pug');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,20 +37,20 @@ app.use('/users', usersRouter);
 app.use(allowCrossDomain);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  console.log("Hello response");
-  next(createError(404));
+app.use(function (req, res, next) {
+    console.log("Hello response");
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- territoire-core/public/javascripts/script_onclick.js
+++ territoire-core/public/javascripts/script_onclick.js
@@ -1,18 +1,16 @@
-function map_onclick (evt)
-{    
-    var features = new Array();    
-    if(map.getView().getResolution() <= 1)
-    {
-	map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {	    
-	    features.push(feature);	
-	},{
-	    layerFilter: function(layer) {
-		return ( (layer.get('name') === 'office' && layer.getVisible()) );
-	    }});
-	var arraylength = features.length;
-	if(arraylength > 0)
-	{
-	    alert('feature selected on click: ' + arraylength);
-	}
+function map_onclick(evt) {
+    var features = new Array();
+    if (map.getView().getResolution() <= 1) {
+        map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+            features.push(feature);
+        }, {
+            layerFilter: function (layer) {
+                return ((layer.get('name') === 'office' && layer.getVisible()));
+            }
+        });
+        var arraylength = features.length;
+        if (arraylength > 0) {
+            alert('feature selected on click: ' + arraylength);
+        }
     }
 }
