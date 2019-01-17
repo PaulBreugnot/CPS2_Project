b112072851234267235e162d4b82bcdbcb104eb9
Paul Breugnot <paul.breugnot@etu.emse.fr>
2019-01-17 10:52:34 +0100

    updated doc : observations

--- dataflow/src/main/java/emse/cps2project/dataflow/model/room/dao/RoomDao.java
+++ dataflow/src/main/java/emse/cps2project/dataflow/model/room/dao/RoomDao.java
@@ -1,9 +1,9 @@
 package emse.cps2project.dataflow.model.room.dao;
 
-import emse.cps2project.dataflow.model.room.ItmRoom;
+import emse.cps2project.dataflow.model.room.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItmRoomDao extends JpaRepository<ItmRoom, Integer> {
+public interface ItmRoomDao extends JpaRepository<Room, Integer> {
 }
